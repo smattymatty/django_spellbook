cf44ddd6fd88fa01e22039bd166b7e0faa7d91f1
smattymatty <smattymatt@gmail.com>
2025-04-29 12:10:53 -0400

    automated content reading for word_count and reading_time on metadata for users.

---
+++ django_spellbook/markdown/content_metrics.py
@@ -0,0 +1,13 @@
+# django_spellbook/markdown/content_metrics.py
+
+def get_word_count(content: str) -> int:
+    """Get word count of content"""
+    return len(content.split())
+
+
+def get_reading_time_minutes(content: str) -> int:
+    """Get reading time in minutes of content"""
+    result = len(content.split()) / 215
+    if result < 1:
+        return 1
+    return result
