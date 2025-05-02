06fcc71ff1364df3a9e394763c74738ce8c195c7
smattymatty <smattymatt@gmail.com>
2025-05-02 16:15:53 -0400

    fixes get_word_count and added tests to ensure it ignored spellbock/django-like definitions, while still counting the content inside.

---
+++ django_spellbook/views_test_app.py
@@ -0,0 +1,5 @@
+import datetime
+from django.shortcuts import render
+
+# Table of Contents for test_app
+TOC = {}
--- tests/test_content_metrics.py
+++ tests/test_content_metrics.py
@@ -2,6 +2,9 @@ import unittest
 from django_spellbook.markdown.content_metrics import get_word_count, get_reading_time_minutes
 
 class TestContentMetrics(unittest.TestCase):
+    def test_get_word_count_empty_string(self):
+        self.assertEqual(get_word_count(''), 0)
+    
     def test_get_word_count(self):
         self.assertEqual(get_word_count('This is a test'), 4)
         self.assertEqual(get_word_count('This is a test.'), 4)
@@ -13,3 +16,36 @@ class TestContentMetrics(unittest.TestCase):
             """
             Wumbo, This is a test.
             """*100), 2)
+        
+    def test_get_word_count_with_spellblock(self):
+        self.assertEqual(get_word_count(
+            '''
+            {~ accordion title="Test" ~}
+            This is a test.
+            {~ accordion ~}
+            '''), 4)
+        
+        self.assertEqual(get_word_count(
+            '''
+            {~ card title="Test" footer="Footer" ~}
+            This is a test.
+            {~ card ~}
+            '''), 4)
+        
+    def test_get_word_count_with_django_tags(self):
+        self.assertEqual(get_word_count(
+            '''
+            {% div %}
+            This is a test.
+            {% enddiv %}
+            '''), 4)
+        
+        self.assertEqual(get_word_count(
+            '''
+            {% div %}
+            This is a test.
+            {% enddiv %}
+            {% div %}
+            Another test.
+            {% enddiv %}
+            '''), 6)
