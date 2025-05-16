035086db6f27b9f4ad4c8c167de67bab103a89b2
smattymatty <smattymatt@gmail.com>
2025-05-16 00:07:56 -0400

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='django-spellbook',
-    version='0.1.14b7',  # Increment version
+    version='0.1.14b9',  # Increment version
     author='Mathew Storm',
     author_email='mathewstormdev@gmail.com',
     description='A Django library for creating and managing content blocks in markdown for developers and bloggers',
